60812471b5796d4ae92782017cbf27486f129330
renaldie <renega99@gmail.com>
2025-04-22 15:37:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,29 +27,29 @@ def main():
     # Add a simple credit at the bottom
     st.markdown("---")
     
-    cols = st.columns([2, 1, 2])
-    with cols[1]:
-        st.markdown(
-            """
-            <div style="text-align: center">
-                © 2025 Resume Relief<br>
-                Made by Team 1 in Hsinchu with ❤️<br>
-            </div>
-            """, 
-            unsafe_allow_html=True
-        )
-
     # cols = st.columns([2, 1, 2])
     # with cols[1]:
     #     st.markdown(
     #         """
     #         <div style="text-align: center">
     #             © 2025 Resume Relief<br>
-    #             Made by <a href="https://renaldi-ega.notion.site" target="_blank">Ren</a> in Hsinchu with ❤️<br>
+    #             Made by Team 1 in Hsinchu with ❤️<br>
     #         </div>
     #         """, 
     #         unsafe_allow_html=True
     #     )
+
+    cols = st.columns([2, 1, 2])
+    with cols[1]:
+        st.markdown(
+            """
+            <div style="text-align: center">
+                © 2025 Resume Relief<br>
+                Made by <a href="https://renaldi-ega.notion.site" target="_blank">Ren</a> in Hsinchu with ❤️<br>
+            </div>
+            """, 
+            unsafe_allow_html=True
+        )
 
 if __name__ == "__main__":
     main()
