12cccddebc855915bb6bdc815dacb0340c5a49f7
renaldie <renega99@gmail.com>
2025-04-23 23:31:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,38 +16,35 @@ st.set_page_config(
 )
 
 # Main app
-def main():
-    home_page = st.Page("pages/home_page.py", title="Home", icon="🪴")
-    page_1 = st.Page("pages/1_✨_Job_Insights.py", title="Job Insights", icon="✨")
-    page_2 = st.Page("pages/2_🔎_Resume_Analysis.py", title="Resume Analysis", icon="🔎")
+home_page = st.Page("pages/home_page.py", title="Home", icon="🪴")
+page_1 = st.Page("pages/1_✨_Job_Insights.py", title="Job Insights", icon="✨")
+page_2 = st.Page("pages/2_🔎_Resume_Analysis.py", title="Resume Analysis", icon="🔎")
 
-    pg = st.navigation([home_page, page_1, page_2])
-    # Add a simple credit at the bottom
-    st.markdown("---")
-    
-    # cols = st.columns([2, 1, 2])
-    # with cols[1]:
-    #     st.markdown(
-    #         """
-    #         <div style="text-align: center">
-    #             © 2025 Resume Relief<br>
-    #             Made by Team 1 in Hsinchu with ❤️<br>
-    #         </div>
-    #         """, 
-    #         unsafe_allow_html=True
-    #     )
+pg = st.navigation([home_page, page_1, page_2])
+pg.run()
+# Add a simple credit at the bottom
+st.markdown("---")
 
-    cols = st.columns([2, 1, 2])
-    with cols[1]:
-        st.markdown(
-            """
-            <div style="text-align: center">
-                © 2025 Resume Relief<br>
-                Made by <a href="https://renaldi-ega.notion.site" target="_blank">Ren</a> in Hsinchu with ❤️<br>
-            </div>
-            """, 
-            unsafe_allow_html=True
-        )
+# cols = st.columns([2, 1, 2])
+# with cols[1]:
+#     st.markdown(
+#         """
+#         <div style="text-align: center">
+#             © 2025 Resume Relief<br>
+#             Made by Team 1 in Hsinchu with ❤️<br>
+#         </div>
+#         """, 
+#         unsafe_allow_html=True
+#     )
 
-if __name__ == "__main__":
-    main()
+cols = st.columns([2, 1, 2])
+with cols[1]:
+    st.markdown(
+        """
+        <div style="text-align: center">
+            © 2025 Resume Relief<br>
+            Made by <a href="https://renaldi-ega.notion.site" target="_blank">Ren</a> in Hsinchu with ❤️<br>
+        </div>
+        """, 
+        unsafe_allow_html=True
+    )
