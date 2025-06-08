eed5316f3aaea98ea0622f5220bb2c19a050464c
difersalest <didiersalest@gmail.com>
2025-06-09 04:03:52 +0800

    Added chatbot code (re-upload)
    Re-upload of chatbot code to the branch with personal account.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,36 +19,42 @@ st.set_page_config(
 def main():
     home_page = st.Page("pages/home_page.py", title="Home", icon="🪴")
     page_1 = st.Page("pages/1_jobs_analysis.py", title="Jobs Analysis", icon="✨")
+    
+    #Uncomment this after merging
     page_2 = st.Page("pages/2_jobs_recommendation.py", title="Jobs Recommendation", icon="🔎")
-
-    pg = st.navigation([home_page, page_1, page_2])
+    page_3 = st.Page("pages/3_job_seeking_coach.py", title="Job Seeking Coach", icon="👩🏻‍💼")
+    #Uncomment this one as well
+    pg = st.navigation([home_page, page_1, page_2, page_3])
+    
+    #pg = st.navigation([home_page, page_1, page_3])
     pg.run()
 
     st.markdown("---")
     
     # cols = st.columns([2, 1, 2])
     # with cols[1]:
-    #     st.markdown(
-    #         """
-    #         <div style="text-align: center">
-    #             © 2025 Resume Relief<br>
-    #             Made by Team 1 in Hsinchu with ❤️<br>
-    #         </div>
-    #         """, 
-    #         unsafe_allow_html=True
-    #     )
-
-    cols = st.columns([2, 1, 2])
-    with cols[1]:
+    with st.sidebar:
         st.markdown(
             """
             <div style="text-align: center">
                 © 2025 Resume Relief<br>
-                Made by <a href="https://renaldi-ega.notion.site" target="_blank">Ren</a> in Hsinchu with ❤️<br>
+                Made by Team 1 in Hsinchu with ❤️<br>
             </div>
             """, 
             unsafe_allow_html=True
         )
+
+    # cols = st.columns([2, 1, 2])
+    # with cols[1]:
+    #     st.markdown(
+    #         """
+    #         <div style="text-align: center">
+    #             © 2025 Resume Relief<br>
+    #             Made by <a href="https://renaldi-ega.notion.site" target="_blank">Ren</a> in Hsinchu with ❤️<br>
+    #         </div>
+    #         """, 
+    #         unsafe_allow_html=True
+    #     )
 
 if __name__ == "__main__":
     main()
